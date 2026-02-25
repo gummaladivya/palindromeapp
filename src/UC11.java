3beacb1a079b00adad116e9a6df96ca8fa4d4d68
gummaladivya <gd1787@srmist.edu.in>
2026-02-25 10:02:39 +0530

    UC11 OOP PALINDROME SERVICE IMPLEMENTED

---
+++ src/UC11.java
@@ -0,0 +1,37 @@
+public class UC11 {
+
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        // Create service object
+        PalindromeService service = new PalindromeService();
+
+        boolean isPalindrome = service.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
+
+// Service class that contains palindrome logic
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
