21ba9b9d30d536c57b22e9af40a0d531398df1ab
gummaladivya <gd1787@srmist.edu.in>
2026-02-25 09:56:52 +0530

    UC10 normalized palindrome implemented

---
+++ src/UC10.java
@@ -0,0 +1,29 @@
+public class UC10 {
+
+    public static void main(String[] args) {
+
+        String input = "A man a plan a canal Panama";
+
+        // Step 1: Normalize string
+        String normalized = input
+                .toLowerCase()                 // convert to lowercase
+                .replaceAll("[^a-z0-9]", "");  // remove spaces & symbols
+
+        boolean isPalindrome = true;
+
+        // Step 2: Compare characters from both ends
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) !=
+                    normalized.charAt(normalized.length() - 1 - i)) {
+
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
